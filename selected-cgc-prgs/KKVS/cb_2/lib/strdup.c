72ee4bdd7a06d06931b78bfb5d813bd49923ef9d
pansilup <pansilu.17@cse.mrt.ac.lk>
2021-09-10 00:05:48 -0400

    adding KKVS prg and its ktest

---
+++ selected-cgc-prgs/KKVS/cb_2/lib/strdup.c
@@ -0,0 +1,14 @@
+#include "cgc_stdlib.h"
+#include "cgc_string.h"
+
+char *cgc_strdup(const char *str)
+{
+    cgc_size_t n = cgc_strlen(str) + 1;
+    /* Haoxin */
+    //char *dup = cgc_malloc(n);
+    char *dup = malloc(n);
+    if (dup == NULL)
+        return NULL;
+    cgc_memcpy(dup, str, n);
+    return dup;
+}
